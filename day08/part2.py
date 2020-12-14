b9f99fef2f745851be21845528f5da5e296a9309
Michael Noronha <michaeltnoronha@gmail.com>
2020-12-14 02:42:24 -0600

    day 8

---
+++ day08/part2.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+highest = 0
+regs = defaultdict(int)
+
+with open("input.txt") as f:
+    for line in f:
+        splt = line.strip().split()
+
+        tgt = splt[0]
+        instr = splt[1]
+        if instr == "inc":
+            val = int(splt[2])
+        else:
+            val = -int(splt[2])
+        condreg = splt[4]
+        cond = splt[5]
+        condval = int(splt[6])
+
+        exp = f"regs[\"{condreg}\"] {cond} {condval}"
+        if eval(exp):
+            regs[tgt] += val
+            highest = max(highest, regs[tgt])
+
+print(highest)
